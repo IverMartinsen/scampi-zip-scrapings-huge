d1032590e911d228cf2ee83a2b66f442091bb0fa
Iver Martinsen <iver.martinsen@outlook.com>
2024-11-06 20:05:27 +0100

    cluster the whole set

--- clustering.py
+++ clustering.py
@@ -1,12 +1,34 @@
+import os
 import h5py
-
-path_to_features = "/Users/ima029/Desktop/SCAMPI/Repository/data/zip scrapings (huge)/features.hdf5"
-
-with h5py.File(path_to_features, 'r') as f:
-    features = f['features'][:]
-    filenames = f['filenames'][:]
-
+import numpy as np
 from sklearn.cluster import MiniBatchKMeans
+
+path_to_features = [os.path.join("embeddings", path) for path in os.listdir("embeddings")]
+path_to_images = [os.path.join("./run_2024-02-08-hdf5-test", path) for path in os.listdir("./run_2024-02-08-hdf5-test")]
+
+# match the features with the images
+paths = []
+for path in path_to_features:
+    shard = os.path.basename(path).split(".")[0]
+    for image_path in path_to_images:
+        if shard == os.path.basename(image_path).split(".")[0]:
+            paths.append((shard, path, image_path))
+
+shards = []
+features = []
+filenames = []
+
+for path in paths:
+    shard, feat_path, file_path = path
+    with h5py.File(feat_path, 'r') as f:
+        features.append(f['features'][:])
+    with h5py.File(file_path, 'r') as f:
+        filenames.append(list(f.keys()))
+    shards.append([shard] * len(filenames[-1]))
+
+shards = np.concatenate(shards, axis=0)
+features = np.concatenate(features, axis=0)
+filenames = np.concatenate(filenames, axis=0)
 
 n_clusters = 100
 kmeans = MiniBatchKMeans(n_clusters=n_clusters, random_state=0)
@@ -14,37 +36,8 @@ kmeans.fit(features)
 
 labels = kmeans.predict(features)
 
-path_to_images = "/Users/ima029/Desktop/SCAMPI/Repository/data/zip scrapings (huge)/run_2024-02-08-hdf5-test/shard_0.hdf5"
-
-with h5py.File(path_to_images, 'r') as f:
-    keys = list(f.keys())
-
-import numpy as np
-
-
-import torch
-import torchvision
-import matplotlib.pyplot as plt
-
-
-for j in range(100):
-
-    keys_ = filenames[np.where(labels == j)]
-    keys_ = [key.decode() for key in keys_]
-
-    with h5py.File(path_to_images, 'r') as f:
-        images = [f[key][:] for key in keys_]
-
-
-    images = [torchvision.io.decode_jpeg(torch.tensor(image)) for image in images]
-
-
-    fig, axs = plt.subplots(10, 10, figsize=(20, 20))
-
-    for i, ax in enumerate(axs.flatten()):
-        try:
-            ax.imshow(images[i].permute(1, 2, 0))
-        except IndexError:
-            pass
-        ax.axis('off')
-    plt.savefig(f'cluster_{j}.png')
+# save filenames and labels
+with h5py.File("cluster_labels.hdf5", 'w') as f:
+    f.create_dataset('shards', data=shards.astype('S'))
+    f.create_dataset('labels', data=labels)
+    f.create_dataset('filenames', data=filenames.astype('S'))
