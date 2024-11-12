b0635f74a2d85e4f6cf77caf0416e75b6ce6cffe
Iver Martinsen <iver.martinsen@outlook.com>
2024-11-12 12:29:39 +0100

    add argparse

--- move_jpgs_into_shards.py
+++ move_jpgs_into_shards.py
@@ -1,23 +1,26 @@
 import os
-import shutil
 import glob
+import shutil
+import argparse
 
-path = "data/zip scrapings (huge)/run_2024-02-08"
-num_shards = 32
+parser = argparse.ArgumentParser()
+parser.add_argument("--path", type=str, default="./run_2024-02-08 curated")
+parser.add_argument("--num_shards", type=int, default=32)
+args = parser.parse_args()
 
 # list jpg files
-files = [os.path.basename(f) for f in glob.glob(path + "/*.jpg")]
+files = [os.path.basename(f) for f in glob.glob(args.path + "/*.jpg")]
 num_files = len(files)
 
 print(f"Number of files: {num_files}")
 
-batch_size = num_files // num_shards
+batch_size = num_files // args.num_shards
 # make sure the number of files is a multiple of the number of batches
-assert num_files % num_shards == 0, "Number of shards must be a multiple of the number of files"
+assert num_files % args.num_shards == 0, "Number of shards must be a multiple of the number of files"
 
 # move files into shards
-for i in range(num_shards):
-    os.makedirs(path + "/shard_" + str(i), exist_ok=True)
-    for j in range(i, len(files), num_shards):
-        shutil.move(path + "/" + files[j], path + "/shard_" + str(i) + "/" + files[j])
+for i in range(args.num_shards):
+    os.makedirs(args.path + "/shard_" + str(i), exist_ok=True)
+    for j in range(i, len(files), args.num_shards):
+        shutil.move(args.path + "/" + files[j], args.path + "/shard_" + str(i) + "/" + files[j])
         
